f4fac0df9b1fd953455c44d15d6f0f661c08dfd1
usman muhammad hassan <hassanusman5667@gmail.com>
2022-09-09 00:14:39 +0100

    used getServerSideProps to get id using the context.params method

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,9 +1,23 @@
 import { useRouter } from "next/router";
 
-const article = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return <div>This is article {id}</div>;
+const article = ({ article }) => {
+  //   const router = useRouter();
+  //   const { id } = router.query;
+  return <div>This is article {article.id}</div>;
 };
 
 export default article;
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
