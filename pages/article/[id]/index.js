1f5649acdc37343bb497983c7fc07d3f95e65e5f
usman muhammad hassan <hassanusman5667@gmail.com>
2022-09-09 23:15:16 +0100

    used getStaticProps with getStaticPaths to get to nested routes

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 const article = ({ article }) => {
   //   const router = useRouter();
@@ -17,7 +16,7 @@ const article = ({ article }) => {
 
 export default article;
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   );
@@ -28,5 +27,20 @@ export const getServerSideProps = async (context) => {
     props: {
       article,
     },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
   };
 };
